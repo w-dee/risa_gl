01d50a0b2767fca41e63058a53157020beeb359d
alfeim <alfeim@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2007-06-17 13:46:52 +0000

    一通りのブレンド関数テンプレートを実装 テストはまだ

--- include/operators/primitive/primitive.hpp
+++ include/operators/primitive/primitive.hpp
@@ -2,13 +2,18 @@
 #define RISA_PRIMITIVE_HPP_
 #include "iterator.hpp"
 #include "risa_types.hpp"
+#include "saturation.hpp"
 
 namespace risa_gl
 {
 	namespace primitive
 	{
-		/**
+		/*
 		 * ファクターテンプレート
+		 */
+
+		/**
+		 * identity 変化させない値を返す
 		 */
 		class identity_factor
 		{
@@ -22,6 +27,9 @@ namespace risa_gl
 			}
 		};
 
+		/**
+		 * zero 値をゼロにする
+		 */
 		class zero_factor
 		{
 		public:
@@ -34,6 +42,9 @@ namespace risa_gl
 			}
 		};
 
+		/**
+		 * ソースのアルファ値を返す
+		 */
 		class source_alpha_factor
 		{
 		public:
@@ -46,6 +57,9 @@ namespace risa_gl
 			}			
 		};
 
+		/**
+		 * ソースのアルファ値を不透明値から引いて返す
+		 */
 		class one_minus_source_alpha_factor
 		{
 		public:
@@ -58,6 +72,9 @@ namespace risa_gl
 			}			
 		};
 
+		/**
+		 * デスティネーションのアルファ値を返す
+		 */
 		class destination_alpha_factor
 		{
 		public:
@@ -70,6 +87,9 @@ namespace risa_gl
 			}
 		};
 
+		/**
+		 * デスティネーションのアルファ値を不透明値から引いて返す
+		 */
 		class one_minus_destination_alpha_factor
 		{
 		public:
@@ -84,6 +104,10 @@ namespace risa_gl
 
 		/**
 		 * ブレンドテンプレート
+		 */
+
+		/**
+		 * 混合テンプレート(サチュレーションなし)
 		 */
 		template <typename source_factor,
 				  typename source_alpha_factor,
@@ -119,73 +143,13 @@ namespace risa_gl
 		};
 
 		/**
-		 *・clear
-		 *色、透過成分ともに初期化。
-		 *Cd = 0
-		 *Ad = 0
+		 * 混合テンプレート(サチュレーションあり)
 		 */
-		class clear
-		{
-		public:
-			template <typename src_itor_t,
-					  typename dest_itor_t,
-					  typename result_itor_t>
-			void operator()(src_itor_t,
-							dest_itor_t,
-							result_itor_t result) const
-			{
-				*result = typename result_itor_t::value_type(0, 0, 0, 0);
-			}
-		};
-
-		/**
-		 * ・src
-		 * ソースの成分をコピー
-		 * Cd = Cs
-		 * Ad = As
-		 */
-		class source
-		{
-		public:
-			template <typename src_itor_t,
-					  typename dest_itor_t,
-					  typename result_itor_t>
-			void operator()(src_itor_t src,
-							dest_itor_t,
-							result_itor_t result) const
-			{
-				*result = *src;
-			}
-		};
-
-		/**
-		 * ・dest
-		 * ディスティネーションの成分をコピー
-		 * Cd = Cd
-		 * Ad = Ad
-		 */
-		class destination
-		{
-		public:
-			template <typename src_itor_t,
-					  typename dest_itor_t,
-					  typename result_itor_t>
-			void operator()(src_itor_t,
-							dest_itor_t dest,
-							result_itor_t result) const
-			{
-				*result = *dest;
-			}
-		};
-
-		/**
-		 * ・src over
-		 * ディスティネーションの上にソースをアルファブレンドでコピー
-		 * # porter-duff source over destination rule.
-		 * Cd = Cs + Cd*(1-As)
-		 * Ad = As + Ad*(1-As)
-		 */
-		class source_over
+		template <typename source_factor,
+				  typename source_alpha_factor,
+				  typename destination_factor,
+				  typename destination_alpha_factor>
+		class blend
 		{
 		public:
 			template <typename src_itor_t,
@@ -195,240 +159,29 @@ namespace risa_gl
 							dest_itor_t dest,
 							result_itor_t result) const
 			{
-				const float fd = (256 - src->get_alpha()) / 256.0f;
-				result->set_red(src->get_red() +
-								dest->get_red() * fd);
-				result->set_green(src->get_green() +
-								  dest->get_green() * fd);
-				result->set_blue(src->get_blue() +
-								 dest->get_blue() * fd);
-				result->set_alpha(src->get_alpha() +
-								  dest->get_alpha() * fd);
+				result->set_red(
+					saturation((src->get_red() *
+								source_factor()(src, dest) +
+								dest->get_red() *
+								destination_factor()(src, dest)) >> 8));
+				result->set_green(
+					saturation((src->get_green() *
+								source_factor()(src, dest) +
+								dest->get_green() *
+								destination_factor()(src, dest)) >> 8));
+				result->set_blue(
+					saturation((src->get_blue() *
+								source_factor()(src, dest) +
+								dest->get_blue() *
+								destination_factor()(src, dest)) >> 8));
+				result->set_alpha(
+					saturation((src->get_alpha() *
+								source_alpha_factor()(src, dest) +
+								dest->get_alpha() *
+								destination_alpha_factor()(src, dest)) >> 8));
 			}
 		};
 
-		/**
-		 * ・dest over
-		 * ソースの上にディスティネーションをアルファブレンドでコピー
-		 * # porter-duff destination over source rule.
-		 * Fs = (1-Ad), Fd=1
-		 * Cd = Cs*(1-Ad) + Cd
-		 * Ad = As*(1-Ad) + Ad
-		 */
-		class destination_over
-		{
-		public:
-			template <typename src_itor_t,
-					  typename dest_itor_t,
-					  typename result_itor_t>
-			void operator()(src_itor_t src,
-							dest_itor_t dest,
-							result_itor_t result) const
-			{
-				const float fs = (256 - dest->get_alpha()) / 256.0f;
-				result->set_red(src->get_red() * fs +
-								dest->get_red());
-				result->set_green(src->get_green() * fs +
-								  dest->get_green());
-				result->set_blue(src->get_blue() * fs +
-								 dest->get_blue());
-				result->set_alpha(src->get_alpha() * fs +
-								  dest->get_alpha());
-			}
-		};
-
-		/**
-		 * ・src in
-		 * ソースをディスティネーションのアルファ成分で処理してコピー
-		 * # porter-duff source in destination rule.
-		 * Fs = Ad, Fd = 0
-		 * Cd = Cs*Ad
-		 * Ad = As*Ad
-		 */
-		class source_in
-		{
-		public:
-			template <typename src_itor_t,
-					  typename dest_itor_t,
-					  typename result_itor_t>
-			void operator()(src_itor_t src,
-							dest_itor_t dest,
-							result_itor_t result) const
-			{
-				const float fs = dest->get_alpha() / 256.0f;
-				result->set_red(src->get_red() * fs);
-				result->set_green(src->get_green() * fs);
-				result->set_blue(src->get_blue() * fs);
-				result->set_alpha(src->get_alpha() * fs);
-			}
-		};
-
-		/**
-		 * ・dest in
-		 * ディスティネーションをソースのアルファ成分で処理してコピー
-		 * # porter-duff destination in source rule.
-		 * Fs = 0, Fd = As
-		 * Cd = Cd*As
-		 * Ad = Ad*As
-		 */
-		class destination_in
-		{
-		public:
-			template <typename src_itor_t,
-					  typename dest_itor_t,
-					  typename result_itor_t>
-			void operator()(src_itor_t src,
-							dest_itor_t dest,
-							result_itor_t result) const
-			{
-				const float fd = src->get_alpha() / 256.0f;
-				result->set_red(dest->get_red() * fd);
-				result->set_green(dest->get_green() * fd);
-				result->set_blue(dest->get_blue() * fd);
-				result->set_alpha(dest->get_alpha() * fd);
-			}
-		};
-
-		/**
-		 * ・src out
-		 * # porter-duff source held out by destination rule.
-		 * Fs = (1-Ad), Fd = 0
-		 * Cd = Cs*(1-Ad)
-		 * Ad = As*(1-Ad)
-		 */
-		class source_out
-		{
-		public:
-			template <typename src_itor_t,
-					  typename dest_itor_t,
-					  typename result_itor_t>
-			void operator()(src_itor_t src,
-							dest_itor_t dest,
-							result_itor_t result) const
-			{
-				const float fs = (256 - dest->get_alpha()) / 256.0f;
-				result->set_red(src->get_red() * fs);
-				result->set_green(src->get_green() * fs);
-				result->set_blue(src->get_blue() * fs);
-				result->set_alpha(src->get_alpha() * fs);
-			}
-		};
-
-		/**
-		 * ・dest out
-		 * porter-duff destination held out by source rule.
-		 * Fs = 0, Fd = (1-As)
-		 * Cd = Cd*(1-As)
-		 * Ad = Ad*(1-As)
-		 */
-		class destination_out
-		{
-		public:
-			template <typename src_itor_t,
-					  typename dest_itor_t,
-					  typename result_itor_t>
-			void operator()(src_itor_t src,
-							dest_itor_t dest,
-							result_itor_t result) const
-			{
-				const float fs = (256 - src->get_alpha()) / 256.0f;
-				result->set_red(dest->get_red() * fs);
-				result->set_green(dest->get_green() * fs);
-				result->set_blue(dest->get_blue() * fs);
-				result->set_alpha(dest->get_alpha() * fs);
-			}
-		};
-
-		/**
-		 * ・src atop
-		 * # porter-duff source atop destination rule.
-		 * Fs = Ad, Fd = (1-As)
-		 * Cd = Cs*Ad + Cd*(1-As)
-		 * Ad = As*Ad + Ad*(1-As) = Ad
-		 */
-		class source_atop
-		{
-		public:
-			template <typename src_itor_t,
-					  typename dest_itor_t,
-					  typename result_itor_t>
-			void operator()(src_itor_t src,
-							dest_itor_t dest,
-							result_itor_t result) const
-			{
-				const float fs = dest->get_alpha() / (256.0f * 2.0f);
-				const float fd = (256 - src->get_alpha()) / (256.0f * 2.0f);
-
-				result->set_red(src->get_red() * fs +
-								dest->get_red() * fd);
-				result->set_green(src->get_green() * fs +
-								  dest->get_green() * fd);
-				result->set_blue(src->get_blue() * fs +
-								 dest->get_blue() * fd);
-				result->set_alpha(dest->get_alpha());
-			}
-		};
-
-		/**
-		 * ・Dest Atop
-		 * # porter-duff destination atop source rule.
-		 * Fs = (1-Ad), Fd = As
-		 * Cd = Cs*(1-Ad) + Cd*As
-		 * Ad = As*(1-Ad) + Ad*As = As
-		 */
-		class destination_atop
-		{
-		public:
-			template <typename src_itor_t,
-					  typename dest_itor_t,
-					  typename result_itor_t>
-			void operator()(src_itor_t src,
-							dest_itor_t dest,
-							result_itor_t result) const
-			{
-				const float fs = (256 - dest->get_alpha()) / 256.0f;
-				const float fd = src->get_alpha() / 256.0f;
-
-				result->set_red((src->get_red() >> 1) * fs +
-								(dest->get_red() >> 1) * fd);
-				result->set_green((src->get_green() >> 1) * fs +
-								  (dest->get_green() >> 1) * fd);
-				result->set_blue((src->get_blue() >> 1) * fs +
-								 (dest->get_blue() >> 1) * fd);
-				result->set_alpha(src->get_alpha());
-			}
-		};
-
-		/**
-		 * ・xor
-		 * # porter-duff source xor destination rule.
-		 * Fs=(1-Ad), Fd=(1-As)
-		 * Cd = Cs*(1-Ad) + Cd*(1-As)
-		 * Ad = As*(1-Ad) + Ad*(1-As)
-		 */
-		class exclusive_or
-		{
-		public:
-			template <typename src_itor_t,
-					  typename dest_itor_t,
-					  typename result_itor_t>
-			void operator()(src_itor_t src,
-							dest_itor_t dest,
-							result_itor_t result) const
-			{
-				const float fs = (256 - dest->get_alpha()) / 256.0f * 2.0f;
-				const float fd = (256 - src->get_alpha()) / 256.0f * 2.0f;
-
-				result->set_red(src->get_red() * fs +
-								dest->get_red() * fd);
-				result->set_green(src->get_green() * fs +
-								  dest->get_green() * fd);
-				result->set_blue(src->get_blue() * fs +
-								 dest->get_blue() * fd);
-				result->set_alpha(src->get_alpha() * fs +
-								  dest->get_alpha() * fd);
-			}
-		};
 	};
 };
 
