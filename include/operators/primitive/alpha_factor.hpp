32193d0caf9a5dd361d8cea0e09ab83feafde1a0
alfeim <alfeim@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2007-07-18 17:34:58 +0000

    アルファ係数、ターゲットセレクタ実装

---
+++ include/operators/primitive/alpha_factor.hpp
@@ -0,0 +1,52 @@
+#ifndef RISA_ALPHA_FACTOR_HPP_
+#define RISA_ALPHA_FACTOR_HPP_
+
+#include "risa_types.hpp"
+
+
+namespace risa_gl
+{
+	namespace primitive
+	{
+		class identity_alpha_factor
+		{
+		public:
+			template <typename src_itor_t,
+					  typename dest_itor_t>
+			risa_gl::uint32 operator()(src_itor_t src,
+									   dest_itor_t dest) const
+			{
+				return 256;
+			}
+		};
+
+		class zero_alpha_factor
+		{
+		public:
+			template <typename src_itor_t,
+					  typename dest_itor_t>
+			risa_gl::uint32 operator()(src_itor_t src,
+									   dest_itor_t dest) const
+			{
+				return 0;
+			}
+		};
+
+		template <typename selecter,
+				  typename method_selecter>
+		class alpha_factor
+		{
+		public:
+			template <typename src_itor_t,
+					  typename dest_itor_t>
+			risa_gl::uint32 operator()(src_itor_t src,
+									   dest_itor_t dest) const
+			{
+				return method_selecter()(selecter()(src, dest));
+			}
+		};
+	};
+};
+
+#endif /* RISA_ALPHA_FACTOR_HPP_ */
+
--- include/operators/primitive/selecter.hpp
+++ include/operators/primitive/selecter.hpp
@@ -9,7 +9,7 @@ namespace risa_gl
 		/**
 		 * sourceを選択するセレクタ
 		 */
-		class source_target_selecter
+		class source_selecter
 		{
 		public:
 			template <typename source_type, typename destination_type>
@@ -22,118 +22,13 @@ namespace risa_gl
 		/**
 		 * destinationを選択するセレクタ
 		 */
-		class destination_target_selecter
+		class destination_selecter
 		{
 		public:
 			template <typename source_type, typename destination_type>
 			destination_type operator()(source_type, destination_type dest)
 			{
 				return dest;
-			}
-		};
-
-		class zero_method_selecter
-		{
-		public:
-			template <typename iterator_type>
-			int operator()(iterator_type) const
-			{
-				return 0;
-			}
-		};
-
-		class one_method_selecter
-		{
-		public:
-			template <typename iterator_type>
-			int operator()(iterator_type) const
-			{
-				return 1;
-			}
-		};
-
-		class saturate_color_method_selecter
-		{
-		public:
-			template <typename iterator_type>
-			int operator()(iterator_type) const
-			{
-				return 255;
-			}
-		};
-
-		class saturate_alpha_method_selecter
-		{
-		public:
-			template <typename iterator_type>
-			int operator()(iterator_type) const
-			{
-				return 256;
-			}
-		};
-
-		/**
-		 * 対象のイテレータから赤色を取り出すセレクタ
-		 */
-		class get_red_method_selecter
-		{
-		public:
-			template <typename iterator_type>
-			int operator()(iterator_type itor) const
-			{
-				return itor->get_red();
-			}
-		};
-
-		/**
-		 * 対象のイテレータから緑色を取り出すセレクタ
-		 */
-		class get_green_method_selecter
-		{
-		public:
-			template <typename iterator_type>
-			int operator()(iterator_type itor)
-			{
-				return itor->get_green();
-			}
-		};
-
-		/**
-		 * 対象のイテレータから青色を取り出すセレクタ
-		 */
-		class get_blue_method_selecter
-		{
-		public:
-			template <typename iterator_type>
-			int operator()(iterator_type itor)
-			{
-				return itor->get_blue();
-			}
-		};
-
-		/**
-		 * 対象のイテレータからアルファ値を取り出すセレクタ
-		 */
-		class get_alpha_method_selecter
-		{
-		public:
-			template <typename iterator_type>
-			int operator()(iterator_type itor)
-			{
-				return itor->get_alpha();
-			}
-		};
-
-		/**
-		 * 対象のイテレータから不透明度を取り出すセレクタ
-		 */
-		class get_opacity_method_selecter
-		{
-		public:
-			template <typename iterator_type>
-			int operator()(iterator_type itor)
-			{
-				return itor->get_opacity();
 			}
 		};
 	};
