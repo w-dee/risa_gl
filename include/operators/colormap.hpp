af4bb9aef634d526d87189cf0671ab5d28ddef13
alfeim <alfeim@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2007-07-25 05:27:20 +0000

    8bpp版のcolormapも追加。一応6bpp版のテストをスケールだけ修正して用意したけど一発で通っちゃったのでいまいち信用できない・・・

--- include/operators/colormap.hpp
+++ include/operators/colormap.hpp
@@ -17,15 +17,19 @@ namespace risa_gl
 		 * * 65階調(文字描画用) と 256階調(通常用) 
 		 * * opacity=255とopacity=255以外の2種類 
 		 *   ? これ、opacity=255だとcolor copyと変わらんのでは?
+		 *   
 		 * * sourceを完全不透明であると見なし、sourceのアルファ値を保
          *         存して合成
 		 *   r = (s.r * o, s.g * o, s.b * o, s.a)
+		 *   
 		 * * sourceを完全不透明であると見なし、sourceのアルファ値を破
          *    壊して合成 (上のやつより高速)
 		 *   r = (s.r * o, s.g * o, s.b * o, ?)
+		 *   
 		 * * sourceのアルファ値をアルファ合成用のアルファ値と見なして合成
 		 *   r = r* 1-s.a +
 		 *     (s.r * s.a * o, s.g * s.a * o, s.b * s.a * o, s.a * o(?))
+		 *     
 		 * * sourceのアルファ値を加算アルファ合成用のアルファ値と見なして合成
 		 *   r = sat(r +
 		 *     (s.r * s.a * o, s.g * s.a * o, s.b * s.a * o, s.a * o(?)))
@@ -183,6 +187,169 @@ namespace risa_gl
 			/**
 			 * @param src 設定する透過度を持つイテレータ
 			 * @param dest opaque値
+			 * @param result 結果を受け取るイテレータ
+			 */
+			template <typename src_itor_t,
+					  typename dest_itor_t,
+					  typename result_itor_t>
+			void operator()(src_itor_t src,
+							dest_itor_t dest,
+							result_itor_t result) const
+			{
+				blender(src, dest, result);
+			}
+		};
+
+		/**
+		 * destinationを透過性としてカラーマップ処理。
+		 * alphaは不定
+		 * r = color.rgba * scale(dest.a)
+		 */
+		class colormap_transparency
+		{
+		private:
+			typedef blend<dynamic_constant_getter,
+						  zero_getter,
+						  bit_setter,
+						  nop_factor,
+						  destination_alpha_getter,
+						  zero_alpha_factor,
+						  not_calculate_policy>
+			colormap_operator_type;
+			colormap_operator_type blender;
+
+			colormap_transparency();
+		public:
+			colormap_transparency(const pixel& color):
+				blender(dynamic_constant_getter(color.get_bit_representation()))
+			{}
+			
+			/**
+			 * @param src 無視される
+			 * @param dest alpha値をもつイテレータ
+			 * @param result 結果を受け取るイテレータ
+			 */
+			template <typename src_itor_t,
+					  typename dest_itor_t,
+					  typename result_itor_t>
+			void operator()(src_itor_t src,
+							dest_itor_t dest,
+							result_itor_t result) const
+			{
+				blender(src, dest, result);
+			}
+		};
+
+		/**
+		 * destinationがアルファとしてカラーマップ処理。
+		 * alphaは保存
+		 * r = color.rgb * scale(dest.a)
+		 */
+		class colormap_transparency_save_alpha
+		{
+		private:
+			typedef blend<dynamic_constant_getter,
+						  zero_getter,
+						  bit_setter,
+						  nop_factor,
+						  destination_alpha_getter,
+						  zero_alpha_factor,
+						  alpha_calculate_policy<
+				destination_alpha_getter> >
+			colormap_operator_type;
+			colormap_operator_type blender;
+
+			colormap_transparency_save_alpha();
+		public:
+			colormap_transparency_save_alpha(const pixel& color):
+				blender(dynamic_constant_getter(color.get_bit_representation()))
+			{}
+			
+			/**
+			 * @param src 設定する透過度を持つイテレータ
+			 * @param dest alpha値をもつイテレータ
+			 * @param result 結果を受け取るイテレータ
+			 */
+			template <typename src_itor_t,
+					  typename dest_itor_t,
+					  typename result_itor_t>
+			void operator()(src_itor_t src,
+							dest_itor_t dest,
+							result_itor_t result) const
+			{
+				blender(src, dest, result);
+			}
+		};
+
+		/**
+		 * destinationをアルファ値としてカラーマップ-アルファブレンド処理。
+		 * alphaは今のところ破壊される
+		 * r = color.rgb * scale(dest.a) +
+		 *     source.rgb * (1-scale(dest.a))
+		 */
+		class colormap_alpha_blend
+		{
+		private:
+			typedef blend<dynamic_constant_getter,
+						  source_getter,
+						  bit_setter,
+						  nop_factor,
+						  destination_alpha_getter,
+						  invert_destination_alpha_getter,
+						  not_calculate_policy>
+			colormap_operator_type;
+			colormap_operator_type blender;
+
+			colormap_alpha_blend();
+		public:
+			colormap_alpha_blend(const pixel& color):
+				blender(dynamic_constant_getter(color.get_bit_representation()))
+			{}
+			
+			/**
+			 * @param src 設定する透過度を持つイテレータ
+			 * @param dest alpha値を持つイテレータ
+			 * @param result 結果を受け取るイテレータ
+			 */
+			template <typename src_itor_t,
+					  typename dest_itor_t,
+					  typename result_itor_t>
+			void operator()(src_itor_t src,
+							dest_itor_t dest,
+							result_itor_t result) const
+			{
+				blender(src, dest, result);
+			}
+		};
+
+		/**
+		 * destinationをアルファ値としてカラーマップ-加色混合処理。
+		 * r = saturation(color.rgb * scale(dest.a) +
+		 *                source.rgb * scale(dest.a))
+		 */
+		class colormap_add_blend
+		{
+		private:
+			typedef blend<dynamic_constant_getter,
+						  source_getter,
+						  bit_setter,
+						  saturation_factor,
+						  destination_alpha_getter,
+						  destination_alpha_getter,
+						  alpha_calculate_policy<
+				destination_alpha_getter > >
+			colormap_operator_type;
+			colormap_operator_type blender;
+
+			colormap_add_blend();
+		public:
+			colormap_add_blend(const pixel& color):
+				blender(dynamic_constant_getter(color.get_bit_representation()))
+			{}
+			
+			/**
+			 * @param src 加色合成対象のイテレータ
+			 * @param dest alpha値をもつイテレータ
 			 * @param result 結果を受け取るイテレータ
 			 */
 			template <typename src_itor_t,
--- test/colormap_test.cpp
+++ test/colormap_test.cpp
@@ -12,6 +12,10 @@ class colormap_test : public CppUnit::TestFixture
 	CPPUNIT_TEST(colormap_6bpp_transparency_save_alpha_test);
 	CPPUNIT_TEST(colormap_6bpp_alpha_blend_test);
 	CPPUNIT_TEST(colormap_6bpp_add_blend_test);
+	CPPUNIT_TEST(colormap_transparency_test);
+	CPPUNIT_TEST(colormap_transparency_save_alpha_test);
+	CPPUNIT_TEST(colormap_alpha_blend_test);
+	CPPUNIT_TEST(colormap_add_blend_test);
 	CPPUNIT_TEST_SUITE_END();
 
 	template <typename container_type>
@@ -122,6 +126,101 @@ public:
 					  generator<opaque>(opaque(65)));
 
 		operators::colormap_6bpp_transparency oper(pixel(128, 128, 128, 129));
+		oper(pixels.begin(), color_map.begin(), pixels.begin());
+		CPPUNIT_ASSERT(pixels.begin()->get_red() == 128);
+		CPPUNIT_ASSERT(pixels.begin()->get_green() == 128);
+		CPPUNIT_ASSERT(pixels.begin()->get_blue() == 128);
+	}
+
+	// 8bpp
+	void colormap_add_blend_test()
+	{
+		using namespace risa_gl;
+
+		typedef pixel_store<pixel> pixels_store;
+
+		pixels_store pixels(640, 480);
+		pixels_store color_map(640, 480);
+
+		std::generate(pixels.begin(), pixels.end(),
+					  generator<pixel>(pixel(128, 128, 128, 256)));
+		std::generate(color_map.begin(), color_map.end(),
+					  generator<pixel>(pixel(0, 0, 0, 256)));
+
+		operators::colormap_add_blend
+			oper(pixel(255, 255, 255, 129));
+
+		oper(pixels.begin(), color_map.begin(), pixels.begin());
+		CPPUNIT_ASSERT(pixels.begin()->get_red() == 255);
+		CPPUNIT_ASSERT(pixels.begin()->get_green() == 255);
+		CPPUNIT_ASSERT(pixels.begin()->get_blue() == 255);
+		CPPUNIT_ASSERT(pixels.begin()->get_alpha() == 256);
+	}
+
+	void colormap_alpha_blend_test()
+	{
+		using namespace risa_gl;
+
+		typedef pixel_store<pixel> pixels_store;
+
+		pixels_store pixels(640, 480);
+		pixels_store color_map(640, 480);
+
+		std::generate(pixels.begin(), pixels.end(),
+					  generator<pixel>(pixel(128, 128, 128, 256)));
+		std::generate(color_map.begin(), color_map.end(),
+					  generator<pixel>(pixel(0, 0, 0, 256)));
+
+		operators::colormap_alpha_blend
+			oper(pixel(255, 255, 255, 129));
+
+		// src(0.5, 0.5, 0.5, 1.0), color(1.0, 1.0, 1.0, 0.5), opacity(1.0)
+		// r = (1.0, 1.0, 1.0, ?)
+
+		oper(pixels.begin(), color_map.begin(), pixels.begin());
+		CPPUNIT_ASSERT(pixels.begin()->get_red() == 255);
+		CPPUNIT_ASSERT(pixels.begin()->get_green() == 255);
+		CPPUNIT_ASSERT(pixels.begin()->get_blue() == 255);
+	}
+
+	void colormap_transparency_save_alpha_test()
+	{
+		using namespace risa_gl;
+
+		typedef pixel_store<pixel> pixels_store;
+
+		pixels_store pixels(640, 480);
+		pixels_store color_map(640, 480);
+
+		std::generate(pixels.begin(), pixels.end(),
+					  generator<pixel>(pixel(128, 128, 128, 256)));
+		std::generate(color_map.begin(), color_map.end(),
+					  generator<pixel>(pixel(0, 0, 0, 256)));
+
+		operators::colormap_transparency_save_alpha
+			oper(pixel(128, 128, 128, 129));
+		oper(pixels.begin(), color_map.begin(), pixels.begin());
+		CPPUNIT_ASSERT(pixels.begin()->get_red() == 128);
+		CPPUNIT_ASSERT(pixels.begin()->get_green() == 128);
+		CPPUNIT_ASSERT(pixels.begin()->get_blue() == 128);
+		CPPUNIT_ASSERT(pixels.begin()->get_alpha() == 256);
+	}
+
+	void colormap_transparency_test()
+	{
+		using namespace risa_gl;
+
+		typedef pixel_store<pixel> pixels_store;
+
+		pixels_store pixels(640, 480);
+		pixels_store color_map(640, 480);
+
+		std::generate(pixels.begin(), pixels.end(),
+					  generator<pixel>(pixel(128, 128, 128, 256)));
+		std::generate(color_map.begin(), color_map.end(),
+					  generator<pixel>(pixel(0, 0, 0, 256)));
+
+		operators::colormap_transparency oper(pixel(128, 128, 128, 129));
 		oper(pixels.begin(), color_map.begin(), pixels.begin());
 		CPPUNIT_ASSERT(pixels.begin()->get_red() == 128);
 		CPPUNIT_ASSERT(pixels.begin()->get_green() == 128);
