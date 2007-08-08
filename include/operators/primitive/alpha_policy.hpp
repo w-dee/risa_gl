652020454515c93ba0d7271dd7a57641d6bc721b
alfeim <alfeim@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2007-08-08 04:51:05 +0000

    alpha計算ポリシーのビットシフト位置計算のendianness対応

--- include/operators/primitive/alpha_policy.hpp
+++ include/operators/primitive/alpha_policy.hpp
@@ -2,6 +2,7 @@
 #define RISA_ALPHA_POLICY_HPP_
 
 #include <risa_types.hpp>
+#include <endian_traits.hpp>
 
 namespace risa_gl
 {
@@ -55,9 +56,14 @@ namespace risa_gl
 									   src_itor_t src,
 									   dest_itor_t dest) const
 			{
-				return ((calculator()(src, dest)-1) <<
-						(result->alpha_position * 8)) |
-					(bits & ~(0xff << (result->alpha_position * 8)));
+				typedef endian_traits<risa_gl::uint32> endian_traits_t;
+				endian_traits_t converter;
+				const size_t alpha_mask_position =
+					converter.little_to_current_offset(
+						result->alpha_position) * 8;
+
+				return ((calculator()(src, dest)-1) << alpha_mask_position) |
+					(bits & ~(0xff << alpha_mask_position));
 			}
 		};
 	}
