3c9899a592ff443978047167585a38fe83ff3bf3
alfeim <alfeim@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2007-10-04 13:04:56 +0000

    Photoshop互換乗算実装

---
+++ test/ps_mul_blend_test.cpp
@@ -0,0 +1,117 @@
+#include <cppunit/extensions/HelperMacros.h>
+#include <operators/ps_mul_blend.hpp>
+#include <pixel.hpp>
+#include <pixel_store.hpp>
+
+#include <iostream>
+
+class ps_mul_blend_operator_test : public CppUnit::TestFixture
+{
+	CPPUNIT_TEST_SUITE(ps_mul_blend_operator_test);
+	CPPUNIT_TEST(ps_mul_blend_test);
+	CPPUNIT_TEST(ps_mul_blend_save_destination_alpha_test);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	template <typename container_type>
+	struct generator
+	{
+		const container_type container;
+
+		generator(container_type container_):
+			container(container_)
+		{}
+
+		container_type operator()() const
+		{
+			return container;
+		}
+	};
+
+	typedef risa_gl::pixel_store<risa_gl::pixel> pixels_store;
+
+	void ps_mul_blend_test()
+	{
+		using namespace risa_gl;
+
+		pixels_store src(640, 480);
+		pixels_store dest(640, 480);
+		pixels_store result(640, 480);
+
+		std::generate(src.begin(), src.end(),
+					  generator<pixel>(pixel(213, 213, 213, 129)));
+		std::generate(dest.begin(), dest.end(),
+					  generator<pixel>(pixel(182, 41, 41, 256)));
+
+		/**
+		 * (151, 34, 34, 129) , (182, 41, 41, 256)
+		 * (75, 17, 17), (91, 20, 20)
+		 * (166, 37, 37)
+		 */
+		operators::ps_mul_blend_operator oper;
+		oper(src.begin(), dest.begin(), result.begin());
+
+		CPPUNIT_ASSERT(result.begin()->get_red() == 167);
+		CPPUNIT_ASSERT(result.begin()->get_green() == 37);
+		CPPUNIT_ASSERT(result.begin()->get_blue() == 37);
+
+		std::generate(src.begin(), src.end(),
+					  generator<pixel>(pixel(255, 255, 255, 129)));
+		std::generate(dest.begin(), dest.end(),
+					  generator<pixel>(pixel(255, 0, 0, 129)));
+
+		/**
+		 * (255, 0, 0, 129), (255, 0, 0)
+		 * (255, 0, 0)
+		 */
+		oper(src.begin(), dest.begin(), result.begin());
+		CPPUNIT_ASSERT(result.begin()->get_red() == 254);
+		CPPUNIT_ASSERT(result.begin()->get_green() == 0);
+		CPPUNIT_ASSERT(result.begin()->get_blue() == 0);
+	}
+
+	void ps_mul_blend_save_destination_alpha_test()
+	{
+		using namespace risa_gl;
+
+		pixels_store src(640, 480);
+		pixels_store dest(640, 480);
+		pixels_store result(640, 480);
+
+		std::generate(src.begin(), src.end(),
+					  generator<pixel>(pixel(213, 213, 213, 129)));
+		std::generate(dest.begin(), dest.end(),
+					  generator<pixel>(pixel(182, 41, 41, 256)));
+
+		/**
+		 * (151, 34, 34, 129) , (182, 41, 41, 256)
+		 * (75, 17, 17), (91, 20, 20)
+		 * (166, 37, 37)
+		 */
+		operators::ps_mul_blend_save_destination_alpha_operator oper;
+		oper(src.begin(), dest.begin(), result.begin());
+
+		CPPUNIT_ASSERT(result.begin()->get_red() == 167);
+		CPPUNIT_ASSERT(result.begin()->get_green() == 37);
+		CPPUNIT_ASSERT(result.begin()->get_blue() == 37);
+		CPPUNIT_ASSERT(result.begin()->get_alpha() == 256);
+
+		std::generate(src.begin(), src.end(),
+					  generator<pixel>(pixel(255, 255, 255, 129)));
+		std::generate(dest.begin(), dest.end(),
+					  generator<pixel>(pixel(255, 0, 0, 129)));
+
+		/**
+		 * (255, 0, 0, 129), (255, 0, 0)
+		 * (255, 0, 0)
+		 */
+		oper(src.begin(), dest.begin(), result.begin());
+
+		CPPUNIT_ASSERT(result.begin()->get_red() == 254);
+		CPPUNIT_ASSERT(result.begin()->get_green() == 0);
+		CPPUNIT_ASSERT(result.begin()->get_blue() == 0);
+		CPPUNIT_ASSERT(result.begin()->get_alpha() == 129);
+
+	}
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION( ps_mul_blend_operator_test );
