059e054a3578ad0f4fa847f5133e032d180027ed
alfeim <alfeim@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2009-07-14 07:43:50 +0000

    透視投影変換の式まちがえてた・・・

--- include/risa_gl/math/projection.hpp
+++ include/risa_gl/math/projection.hpp
@@ -30,8 +30,8 @@ namespace risa_gl
 								  const value_type& near,
 								  const value_type& far)
 			{
-				const value_type wide_distance = right - left;
-				const value_type wide_center = right + left;
+				const value_type wide_distance = left - right;
+				const value_type wide_center = left + right;
 
 				const value_type height_distance = top - bottom;
 				const value_type height_center = top + bottom;
