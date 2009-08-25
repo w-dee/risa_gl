9aad8d0bf01b4215e8be366897000e1f7dadfddd
alfeim <alfeim@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2009-08-25 10:06:47 +0000

    透視投影空間上での補間クラスの追加 検算用のテストも追加

--- include/risa_gl/render/interpolator.hpp
+++ include/risa_gl/render/interpolator.hpp
@@ -2,50 +2,94 @@
 #define RISA_RENDER_INTERPOLATOR_HPP_
 
 #include <cassert>
+#include <risa_gl/render/xyzw_st_coord.hpp>
 
 namespace risa_gl
 {
 	namespace render
 	{
-		template <typename value_t, typename scalar_t = int>
-		class interpolator
+		template <typename value_t>
+		struct interpolated_values
+		{
+			value_t s;
+			value_t t;
+			value_t z;
+
+			interpolated_values(const value_t& s_,
+								const value_t& t_,
+								const value_t& z_):
+				s(s_), t(t_), z(z_)
+			{}
+		};
+
+		template <typename value_t>
+		class xyzw_st_coord_interpolator
 		{
 		public:
 			typedef value_t value_type;
-			typedef scalar_t scalar_type;
 
 		private:
-			const value_type& head;
-			const value_type& tail;
-			const scalar_type& division;
+			const xyzw_st_coord<value_type>& head;
+			const xyzw_st_coord<value_type>& tail;
 
-			value_type stepping;
+			const value_type& division;
+			const value_type inv_division;
+
+			const value_type s0z0; // S[0] / Z[0]
+			const value_type s1z1; // S[1] / Z[1]
+			const value_type distance_s0z0_s1z1; // S[1] / Z[1] - S[0] / Z[0]
+			const value_type t0z0; // T[0] / Z[0]
+			const value_type t1z1; // T[1] / Z[1]
+			const value_type distance_t0z0_t1z1; // T[1] / Z[1] - T[0] / Z[0]
+			const value_type inv_z0; // 1 / Z[0]
+			const value_type inv_z1; // 1 / Z[1]
+			const value_type distance_inv_z0_inv_z1; // 1/Z[1] - 1/Z[0]
 			
 		public:
-			interpolator(const value_type& head_,
-						 const value_type& tail_,
-						 const scalar_type& division_):
+			xyzw_st_coord_interpolator(const xyzw_st_coord<value_type>& head_,
+									   const xyzw_st_coord<value_type>& tail_,
+									   const value_type& division_):
 				head(head_), tail(tail_),
+
 				division(division_),
-				stepping((tail_ - head_) / division_)
+				inv_division(1 / division_),
+
+				s0z0(head_.get_s() / head_.get_z()),
+				s1z1(tail_.get_s() / tail_.get_z()),
+				distance_s0z0_s1z1(tail_.get_s() / tail_.get_z() -
+								   head_.get_s() / head_.get_z()),
+
+				t0z0(head_.get_t() / head_.get_z()),
+				t1z1(tail_.get_t() / tail_.get_z()),
+				distance_t0z0_t1z1(tail_.get_t() / tail_.get_z() -
+								   head_.get_t() / head_.get_z()),
+
+				inv_z0(1 / head_.get_z()),
+				inv_z1(1 / tail_.get_z()),
+				distance_inv_z0_inv_z1(1 / tail_.get_z() - 1 / head_.get_z())
 			{}
 
-			scalar_type get_division() const
+			value_type get_division() const
 			{
 				return division;
 			}
 				
 
-			value_type interpolate(const scalar_type& step) const
+			interpolated_values<value_type>
+			interpolate(const value_type& step) const
 			{
-				assert (step < division);
+				assert (step <= division);
 
-				return head + stepping * step;
-			}
+				const value_type s = step * inv_division;
 
-			value_type get_stepping() const
-			{
-				return stepping;
+				const value_type z_s =
+					1 / (inv_z0 + s * distance_inv_z0_inv_z1);
+
+				return
+					interpolated_values<value_type>
+					((s0z0 + s * distance_s0z0_s1z1) * z_s,
+					 (t0z0 + s * distance_t0z0_t1z1) * z_s,
+					 z_s);
 			}
 		};
 	}
--- test/render_interpolator_test.cpp
+++ test/render_interpolator_test.cpp
@@ -3,76 +3,39 @@
 #include <risa_gl/math/point.hpp>
 #include <risa_gl/math/fraction.hpp>
 
+#include <iostream>
+
 class render_interpolator_test : public CppUnit::TestFixture
 {
 	CPPUNIT_TEST_SUITE(render_interpolator_test);
 	CPPUNIT_TEST(coord_interpolate_test);
-	CPPUNIT_TEST(fraction_coord_interpolate_test);
-	CPPUNIT_TEST(fraction_stepping_interpolate_test);
 	CPPUNIT_TEST_SUITE_END();
 
 public:
 	void coord_interpolate_test()
 	{
-		using risa_gl::math::point;
-		using risa_gl::render::interpolator;
+		using risa_gl::render::xyzw_st_coord;
+		using risa_gl::render::xyzw_st_coord_interpolator;
 
-		typedef point<int> point_t;
-		interpolator<point_t, int>
-			coord_interpolator(point_t(0, 0, 0),
-							   point_t(10, 20, 30),
+		typedef xyzw_st_coord<float> point_t;
+		xyzw_st_coord_interpolator<float>
+			coord_interpolator(point_t(0.f, 0.f, 1.f, 1.f, 0.f, 0.f),
+							   point_t(10.f, 20.f, 30.f, 1.f, 1.f, .5f),
 							   10);
 
-		point_t step = coord_interpolator.get_stepping();
-		CPPUNIT_ASSERT_EQUAL(step.get_x(), 1);
-		CPPUNIT_ASSERT_EQUAL(step.get_y(), 2);
-		CPPUNIT_ASSERT_EQUAL(step.get_z(), 3);
-	}
-
-	void fraction_coord_interpolate_test()
-	{
-		using risa_gl::math::point;
-		using risa_gl::math::fraction;
-		using risa_gl::render::interpolator;
-
-		typedef fraction<int> fraction_type;
-		typedef point<fraction_type> point_t;
-		interpolator<point_t, fraction_type>
-			coord_interpolator(point_t(fraction_type(0), fraction_type(0), fraction_type(0)),
-							   point_t(fraction_type(10), fraction_type(20), fraction_type(30)),
-							   fraction_type(10));
-
-		point_t step = coord_interpolator.get_stepping();
-		CPPUNIT_ASSERT_EQUAL(step.get_x(), fraction_type(1));
-		CPPUNIT_ASSERT_EQUAL(step.get_y(), fraction_type(2));
-		CPPUNIT_ASSERT_EQUAL(step.get_z(), fraction_type(3));
-	}
-
-	void fraction_stepping_interpolate_test()
-	{
-		using risa_gl::math::point;
-		using risa_gl::math::fraction;
-		using risa_gl::render::interpolator;
-
-		typedef fraction<int> fraction_type;
-		typedef point<fraction_type> point_t;
-		const int division = 100;
-		interpolator<point_t, fraction_type>
-			coord_interpolator(point_t(fraction_type(0), fraction_type(0), fraction_type(0)),
-							   point_t(fraction_type(10), fraction_type(20), fraction_type(30)),
-							   fraction_type(division));
-
-		for (int offset = 0;
-			 offset < coord_interpolator.get_division().get_value();
-			 ++offset)
+		for (int offset = 0; offset <= 10; ++offset)
 		{
-			point_t val = coord_interpolator.interpolate(fraction_type(offset));
+			risa_gl::render::interpolated_values<float> result =
+				coord_interpolator.interpolate(offset);
 
-			CPPUNIT_ASSERT_EQUAL(val.get_x(), fraction_type(10 * offset, 100));
-			CPPUNIT_ASSERT_EQUAL(val.get_y(), fraction_type(20 * offset, 100));
-			CPPUNIT_ASSERT_EQUAL(val.get_z(), fraction_type(30 * offset, 100));
+			std::cout << "(" <<
+				result.s << ", " <<
+				result.t << ", " <<
+				result.z << ") " << std::endl;
 		}
 	}
+
+
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION( render_interpolator_test );
